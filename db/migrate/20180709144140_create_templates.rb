83ce74f0566ffbabaaac00eda20c51e3bfa6d709
leekyeongseo <nemo513@naver.com>
2018-07-10 22:09:03 +0900

    user-template,  1:n relation

--- db/migrate/20180709144140_create_templates.rb
+++ db/migrate/20180709144140_create_templates.rb
@@ -3,6 +3,7 @@ class CreateTemplates < ActiveRecord::Migration[5.1]
     create_table :templates do |t|
       t.string :title
       t.text :desc
+      t.belongs_to :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20180709150435) do
   create_table "templates", force: :cascade do |t|
     t.string "title"
     t.text "desc"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_templates_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
