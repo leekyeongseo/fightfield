3dc148ff726beb023bab2af6fc08c148b3a57f1b
qwerty98765 <maeng9584@naver.com>
2018-07-14 17:15:33 +0900

    disgard

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :templates
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'templates#index'
+  get '/', to: 'feeds#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
