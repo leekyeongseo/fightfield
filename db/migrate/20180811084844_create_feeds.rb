def1a8e5a7b9acd3d49fe76583f11accd65c5061
leekyeongseo <nemo513@naver.com>
2018-08-11 17:53:58 +0900

    feed model edited

--- db/migrate/20180811084844_create_feeds.rb
+++ db/migrate/20180811084844_create_feeds.rb
@@ -3,7 +3,7 @@ class CreateFeeds < ActiveRecord::Migration[5.1]
     create_table :feeds do |t|
       t.string :to
       t.string :from
-      t.string :value
+      t.string :type
       t.text :content
 
       t.timestamps
