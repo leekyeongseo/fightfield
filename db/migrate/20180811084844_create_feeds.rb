a8c226152ece2ed0a2cef45265f051f55f8091ff
leekyeongseo <nemo513@naver.com>
2018-08-11 17:52:07 +0900

    feed model

---
+++ db/migrate/20180811084844_create_feeds.rb
@@ -0,0 +1,12 @@
+class CreateFeeds < ActiveRecord::Migration[5.1]
+  def change
+    create_table :feeds do |t|
+      t.string :to
+      t.string :from
+      t.string :value
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
